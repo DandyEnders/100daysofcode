080fc781a776d13b92b8eb2480326ec64c4c2f42
DandyEnders <howden2@gmail.com>
2022-04-25 22:10:14 +0900

    Added day2 main + bmi

---
+++ day2/main.py
@@ -0,0 +1,39 @@
+# Data Types
+
+# String
+
+print("Hello"[4])
+
+print("123" + "345")
+
+# Integer
+
+print(123 + 345)
+
+print(123_456_789)
+
+# Float
+
+3.14159
+
+734_529.678 # Wow!
+
+# Boolean
+
+True
+False
+
+
+two_digit_number = input("Type a two digit number: ")
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+result = int(first_digit) + int(second_digit)
+print(result)
+
+print( 2 ** 3) # power
+
+print("abc")
+
+
+
+
