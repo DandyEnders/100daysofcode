79b3ae0ab7498499247bdc9750dc9995a89f05ab
DandyEnders <howden2@gmail.com>
2022-04-30 11:41:18 +0900

    Implement challenge 3

--- day7/Hangman.py
+++ day7/Hangman.py
@@ -8,11 +8,14 @@ print(f"Pssst, The solution is {chosen_word}")
 word_line = ["_"] * len(chosen_word)
 
 print(word_line)
+end_of_game = False
+while not end_of_game:
+  guess_letter = input("Guess a letter: ").lower()
 
-guess_letter = input("Guess a letter: ").lower()
-
-for i, l in enumerate(chosen_word):
-  if l == guess_letter:
-    word_line[i] = l
-    
-print(word_line)
+  for i, l in enumerate(chosen_word):
+    if l == guess_letter:
+      word_line[i] = l
+      
+  print(word_line)
+  if "_" not in word_line:
+    end_of_game = True
