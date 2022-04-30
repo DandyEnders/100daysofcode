03aef82f5a86fbc2fcac891e5b4f4b442b9cec9c
DandyEnders <howden2@gmail.com>
2022-04-30 11:07:44 +0900

    Implement challenge 1

--- day7/Hangman.py
+++ day7/Hangman.py
@@ -1,2 +1,14 @@
+import random
+
+
 word_list = ["ardvark", "baboon", "camel"]
 
+chosen_word = random.choice(word_list)
+
+guess_letter = input("Guess a letter: ")
+
+for l in chosen_word:
+  if l == guess_letter:
+    print(True)
+  else:
+    print(False)
