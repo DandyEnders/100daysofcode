89445a1e4781169c638eb773cf7e3a0516f10ae2
DandyEnders <howden2@gmail.com>
2022-04-30 11:34:56 +0900

    Implement challenge 2

--- day7/Hangman.py
+++ day7/Hangman.py
@@ -1,14 +1,24 @@
 import random
-
+def print_list(lst: list):
+  result = '["'
+  result += '", "'.join(lst)
+  result += '"]'
+  print(result)
+    
 
 word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
+print(f"Pssst, The solution is {chosen_word}")
+
+word_line = ["_"] * len(chosen_word)
+
+print(word_line)
 
 guess_letter = input("Guess a letter: ").lower()
 
-for l in chosen_word:
+for i, l in enumerate(chosen_word):
   if l == guess_letter:
-    print(True)
-  else:
-    print(False)
+    word_line[i] = l
+    
+print(word_line)
