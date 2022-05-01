3970d4a8ea1ee1f93b66988c9709fc7c65d7ed12
DandyEnders <howden2@gmail.com>
2022-05-01 12:20:23 +0900

    Implement day-8-start

---
+++ day8/main.py
@@ -0,0 +1,11 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the funciton.
+# Call the greet () function and run your code.
+
+def greet():
+  print("Hello!")
+  print("How are you?")
+  print("I am fine, thank you.")
+
+greet()
