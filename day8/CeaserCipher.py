f0d1dabb736de207ddf8272d2c36985a2a945b02
DandyEnders <howden2@gmail.com>
2022-05-01 13:10:47 +0900

    Implement TD-1

--- day8/CeaserCipher.py
+++ day8/CeaserCipher.py
@@ -1,4 +1,7 @@
+import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(art.logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
