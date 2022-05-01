923637b470326e898f39a83a75e6135a38f273f1
DandyEnders <howden2@gmail.com>
2022-05-01 13:08:35 +0900

    Replace encrypt, decrypt with caesar

--- day8/CeaserCipher.py
+++ day8/CeaserCipher.py
@@ -4,29 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for letter in start_text:
     position = alphabet.index(letter)
     new_position = (position + shift_amount) % 26
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
-    
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = (position - shift_amount) % 26
-    new_letter = alphabet[new_position]
-    plain_text += new_letter
-  print(f"The decrypted text is {plain_text}")
-
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}d text is {end_text}")
 
 if direction == "encode":
-  encrypt(text, shift)
+  caesar(text, shift, direction)
 elif direction == "decode":
-  decrypt(text, shift)
+  caesar(text, shift, direction)
 else:
   print("Only 'encode' and 'decode' allowed.")
