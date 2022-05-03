d793a696dfcafd9a7a776fdcaf14c7ca1e11bf35
DandyEnders <howden2@gmail.com>
2022-05-03 11:38:11 +0900

    Add docstring to main.format_name

--- Day10/main.py
+++ Day10/main.py
@@ -1,10 +1,20 @@
 def format_name(first_name, last_name):
+  """Take a first and last name and format it to
+  return the title case version of the name.
+
+  Args:
+      first_name (str): first name
+      last_name (str): last name
+
+  Returns:
+      str: formatted name
+  """
   if first_name == "" or last_name == "":
     return "You did not provide valid inputs."
   formatted_first_name = first_name.title()
   formatted_last_name = last_name.title()
-
-  print(formatted_first_name, formatted_last_name)
+  
+  return formatted_first_name, formatted_last_name
   
 formatted_string = format_name("John", "Doe")
 print(formatted_string)
