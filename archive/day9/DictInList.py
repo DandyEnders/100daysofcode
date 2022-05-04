f2a6017677c56d2ff8ea728c872fd1ea3c2047b7
DandyEnders <howden2@gmail.com>
2022-05-04 11:54:56 +0900

    Change TODO to DONE in DictInList.py

--- archive/day9/DictInList.py
+++ archive/day9/DictInList.py
@@ -15,7 +15,7 @@ travel_log = [
 ]
 #🚨 Do NOT change the code above
 
-#TODO: Write the function that will allow new countries
+# DONE: Write the function that will allow new countries
 #to be added to the travel_log. 👇
 
 def add_new_country(country, visits, cities_list):
