205a24843cafe791ccc6d69ed99e17ddb60640c0
DandyEnders <howden2@gmail.com>
2022-05-05 11:33:56 +0900

    Learn local scope

--- day12/main.py
+++ day12/main.py
@@ -1,9 +1,21 @@
-enemies = 1
+# enemies = 1
 
-def increase_enemies():
-    enemies = 2
-    print(f"enemies inside the function {enemies}")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside the function {enemies}")
 
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# # local scopes
+
+
+# def drink_potion():
+#   potion_strength = 2
+#   print(potion_strength)
+
+
+# drink_potion()
+# print(potion_strength)
