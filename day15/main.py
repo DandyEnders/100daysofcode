f869cc4a2b2045c45f23626e051d4463baaf698e
DandyEnders <howden2@gmail.com>
2022-05-09 17:21:45 +0900

    Add handling for when inserted money is insufficient

--- day15/main.py
+++ day15/main.py
@@ -28,25 +28,40 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0.0,
 }
+
+
 
 
 def is_resources_sufficient_for(input_menu):
     assert input_menu in MENU.keys()
 
-    water, milk, coffee = MENU["input_menu"]["ingredients"].values()
-    return resources.water >= water and resources.milk >= milk and resources.coffee >= coffee
+    ingredient = MENU["input_menu"]["ingredients"]
+    req_water = ingredient["water"]
+    req_milk = ingredient["milk"]
+    req_coffee = ingredient["coffee"]
+    return resources.water >= req_water and resources.milk >= req_milk and resources.coffee >= req_coffee
 
 
 def deficient_resources(input_menu) -> list:
     assert input_menu in MENU.keys()
 
     result = []
-    req_water, req_milk, req_coffee = MENU["input_menu"]["ingredients"].values()
+    ingredient = MENU["input_menu"]["ingredients"]
+    req_water = ingredient["water"]
+    req_milk = ingredient["milk"]
+    req_coffee = ingredient["coffee"]
     if req_water > resources.water: result.append("water")
     if req_milk > resources.milk: result.append("milk")
     if req_coffee > resources.coffee: result.append("coffee")
     return result
+
+
+def is_money_sufficient_for(input_menu, money_paid) -> bool:
+    assert is_resources_sufficient_for(input_menu)
+
+    return MENU[input_menu]["cost"] <= money_paid
 
 
 # DONE: do following while dispensing is not finished:
@@ -79,9 +94,12 @@ while is_machine_on:
     #   input("Insert coin in the format of: #quarters #dimes #nickles #pennies
     elif menu_choice in MENU.keys() and is_resources_sufficient_for(menu_choice):
         quarter, dime, nickle, penny = input("Insert coin in the format of: #quarters #dimes #nickles #pennies: ")
-
-# TODO: if user did not insert enough money,
-#   print("Sorry that's not enough money. Money refunded.")
+        quarter, dime, nickle, penny = int(quarter), int(dime), int(nickle), int(penny)
+        money_total = quarter * 0.25 + dime * 0.1 + nickle * 0.05 + penny * 0.01
+        # DONE: if user did not insert enough money,
+        #   print("Sorry that's not enough money. Money refunded.")
+        if not is_money_sufficient_for(menu_choice, money_total):
+            print("Sorry that's not enough money. Money refunded.")
 # TODO: if user did insert enough money,
 #   reflect money input into amount of money remaining
 # TODO: if user inserted too much money, offer the change
