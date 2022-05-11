70b9b2780d0943b359ff10f6c04a660f57c86d3e
DandyEnders <howden2@gmail.com>
2022-05-11 11:11:21 +0900

    Implement QuizBrain.still_has_questions

--- day17/quiz_project/main.py
+++ day17/quiz_project/main.py
@@ -5,4 +5,4 @@ from quiz_brain import QuizBrain
 question_bank = [Question(q["text"], q["answer"]) for q in question_data]
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+quiz.still_has_questions()
