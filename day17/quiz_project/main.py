82f1c686b976cbc0676b2e86f8e7f23f33d92669
DandyEnders <howden2@gmail.com>
2022-05-11 11:10:55 +0900

    Remove question_bank print

--- day17/quiz_project/main.py
+++ day17/quiz_project/main.py
@@ -3,7 +3,6 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = [Question(q["text"], q["answer"]) for q in question_data]
-print(question_bank)
 
 quiz = QuizBrain(question_bank)
 quiz.next_question()
