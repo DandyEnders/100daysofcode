7de1729c290792c2f507d34a707ad3f6b1b51449
DandyEnders <howden2@gmail.com>
2022-05-18 10:43:32 +0900

    Make Timmy draw a square

--- day18/main.py
+++ day18/main.py
@@ -4,7 +4,12 @@ timmy = Turtle()
 timmy.color("red")
 timmy.forward(100)
 timmy.right(90)
-
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
 
 
 screen = Screen()
